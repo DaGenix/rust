153535ad2f1c35ab664e6b80829c6efc5479a576
Palmer Cox <p@lmercox.com>
2013-09-04 23:58:31 -0400

    Update PBKDF2.

--- src/libextra/crypto/pbkdf2.rs
+++ src/libextra/crypto/pbkdf2.rs
@@ -13,10 +13,15 @@
  * http://tools.ietf.org/html/rfc2898.
  */
 
+use std::rand::{IsaacRng, RngUtil};
 use std::vec;
 
-use cryptoutil::write_u32_be;
+use base64;
+use base64::{FromBase64, ToBase64};
+use cryptoutil::{read_u32_be, write_u32_be, fixed_time_eq};
+use hmac::Hmac;
 use mac::Mac;
+use sha2::Sha256;
 
 // Calculate a block of the output of size equal to the output_bytes of the underlying Mac function
 // mac - The Mac function to use
@@ -28,7 +33,7 @@ use mac::Mac;
 fn calculate_block<M: Mac>(
         mac: &mut M,
         salt: &[u8],
-        c: uint,
+        c: u32,
         idx: u32,
         scratch: &mut [u8],
         block: &mut [u8]) {
@@ -76,7 +81,7 @@ fn calculate_block<M: Mac>(
  * * output - The output buffer to fill with the derived key value.
  *
  */
-pub fn pbkdf2<M: Mac>(mac: &mut M, salt: &[u8], c: uint, output: &mut [u8]) {
+pub fn pbkdf2<M: Mac>(mac: &mut M, salt: &[u8], c: u32, output: &mut [u8]) {
     assert!(c > 0);
 
     let os = mac.output_bytes();
@@ -109,18 +114,156 @@ pub fn pbkdf2<M: Mac>(mac: &mut M, salt: &[u8], c: uint, output: &mut [u8]) {
     }
 }
 
+/**
+ * pbkdf2_simple is a helper function that should be sufficient for the majority of cases where
+ * an application needs to use PBKDF2 to hash a password for storage. The result is a ~str that
+ * contains the parameters used as part of its encoding. The pbkdf2_check function may be used on
+ * a password to check if it is equal to a hashed value.
+ *
+ * # Format
+ *
+ * The format of the output is a modified version of the Modular Crypt Format that encodes algorithm
+ * used and iteration count. The format is indicated as "rpbkdf2" which is short for "Rust PBKF2
+ * format."
+ *
+ * $rpbkdf2$0$<base64(c)>$<base64(salt)>$<based64(hash)>$
+ *
+ * # Arguments
+ *
+ * * password - The password to process as a str
+ * * c - The iteration count
+ *
+ */
+pub fn pbkdf2_simple(password: &str, c: u32) -> ~str {
+    let mut rng = IsaacRng::new();
+
+    // 128-bit salt
+    let salt = rng.gen_bytes(16);
+
+    // 256-bit derived key
+    let mut dk = [0u8, ..32];
+
+    let mut mac = Hmac::new(Sha256::new(), password.as_bytes());
+
+    pbkdf2(&mut mac, salt, c, dk);
+
+    let mut result = ~"$rpbkdf2$0$";
+    let mut tmp = [0u8, ..4];
+    write_u32_be(tmp, c);
+    result.push_str(tmp.slice_to(tmp.len()).to_base64(base64::STANDARD));
+    result.push_char('$');
+    result.push_str(salt.slice_to(salt.len()).to_base64(base64::STANDARD));
+    result.push_char('$');
+    result.push_str(dk.slice_to(dk.len()).to_base64(base64::STANDARD));
+    result.push_char('$');
+
+    return result;
+}
+
+/**
+ * pbkdf2_check compares a password against the result of a previous call to pbkdf2_simple and
+ * returns true if the passed in password hashes to the same value.
+ *
+ * # Arguments
+ *
+ * * password - The password to process as a str
+ * * hashed_value - A string representing a hashed password returned by pbkdf2_simple()
+ *
+ */
+pub fn pbkdf2_check(password: &str, hashed_value: &str) -> Result<bool, &'static str> {
+    static ERR_STR: &'static str = "Hash is not in Rust PBKDF2 format.";
+
+    let mut iter = hashed_value.split_iter('$');
+
+    // Check that there are no characters before the first "$"
+    match iter.next() {
+        Some(x) => if x != "" { return Err(ERR_STR); },
+        None => return Err(ERR_STR)
+    }
+
+    // Check the name
+    match iter.next() {
+        Some(t) => if t != "rpbkdf2" { return Err(ERR_STR); },
+        None => return Err(ERR_STR)
+    }
+
+    // Parse format - currenlty only version 0 is supported
+    match iter.next() {
+        Some(fstr) => {
+            match fstr {
+                "0" => { }
+                _ => return Err(ERR_STR)
+            }
+        }
+        None => return Err(ERR_STR)
+    }
+
+    // Parse the iteration count
+    let c = match iter.next() {
+        Some(pstr) => match pstr.from_base64() {
+            Ok(pvec) => {
+                if pvec.len() != 4 { return Err(ERR_STR); }
+                read_u32_be(pvec)
+            }
+            Err(_) => return Err(ERR_STR)
+        },
+        None => return Err(ERR_STR)
+    };
+
+    // Salt
+    let salt = match iter.next() {
+        Some(sstr) => match sstr.from_base64() {
+            Ok(salt) => salt,
+            Err(_) => return Err(ERR_STR)
+        },
+        None => return Err(ERR_STR)
+    };
+
+    // Hashed value
+    let hash = match iter.next() {
+        Some(hstr) => match hstr.from_base64() {
+            Ok(hash) => hash,
+            Err(_) => return Err(ERR_STR)
+        },
+        None => return Err(ERR_STR)
+    };
+
+    // Make sure that the input ends with a "$"
+    match iter.next() {
+        Some(x) => if x != "" { return Err(ERR_STR); },
+        None => return Err(ERR_STR)
+    }
+
+    // Make sure there is no trailing data after the final "$"
+    match iter.next() {
+        Some(_) => return Err(ERR_STR),
+        None => { }
+    }
+
+    let mut mac = Hmac::new(Sha256::new(), password.as_bytes());
+
+    let mut output = vec::from_elem(hash.len(), 0u8);
+    pbkdf2(&mut mac, salt, c, output);
+
+    // Be careful here - its important that the comparison be done using a fixed time equality
+    // check. Otherwise an adversary that can measure how long this step takes can learn about the
+    // hashed value which would allow them to mount an offline brute force attack against the
+    // hashed password.
+    return Ok(fixed_time_eq(output, hash));
+}
+
 #[cfg(test)]
 mod test {
     use std::vec;
 
-    use pbkdf2::pbkdf2;
+    use pbkdf2::{pbkdf2, pbkdf2_simple, pbkdf2_check};
     use hmac::Hmac;
     use sha1::Sha1;
 
     struct Test {
         password: ~[u8],
         salt: ~[u8],
-        c: uint,
+        c: u32,
         expected: ~[u8]
     }
 
@@ -184,6 +327,36 @@ mod test {
             let mut result = vec::from_elem(t.expected.len(), 0u8);
             pbkdf2(&mut mac, t.salt, t.c, result);
             assert!(result == t.expected);
+        }
+    }
+
+    #[test]
+    fn test_pbkdf2_simple() {
+        let password = "password";
+
+        let out1 = pbkdf2_simple(password, 1024);
+        let out2 = pbkdf2_simple(password, 1024);
+
+        // This just makes sure that a salt is being applied. It doesn't verify that that salt is
+        // cryptographically strong, however.
+        assert!(out1 != out2);
+
+        match pbkdf2_check(password, out1) {
+            Ok(r) => assert!(r),
+            Err(_) => fail!()
+        }
+        match pbkdf2_check(password, out2) {
+            Ok(r) => assert!(r),
+            Err(_) => fail!()
+        }
+
+        match pbkdf2_check("wrong", out1) {
+            Ok(r) => assert!(!r),
+            Err(_) => fail!()
+        }
+        match pbkdf2_check("wrong", out2) {
+            Ok(r) => assert!(!r),
+            Err(_) => fail!()
         }
     }
 }
