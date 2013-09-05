5b66d5d91019c7218998772a65bf3fce9449e607
Palmer Cox <p@lmercox.com>
2013-09-04 23:36:43 -0400

    Update PBKDF2.

--- src/libextra/crypto/pbkdf2.rs
+++ src/libextra/crypto/pbkdf2.rs
@@ -8,12 +8,23 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+/*!
+ * This module implements the PBKDF2 Key Derivation Function as specified by
+ * http://tools.ietf.org/html/rfc2898.
+ */
+
 use std::vec;
 
 use cryptoutil::write_u32_be;
 use mac::Mac;
 
-
+// Calculate a block of the output of size equal to the output_bytes of the underlying Mac function
+// mac - The Mac function to use
+// salt - the salt value to use
+// c - the iteration count
+// idx - the 1 based index of the block
+// scratch - a temporary variable the same length as the block
+// block - the block of the output to calculate
 fn calculate_block<M: Mac>(
         mac: &mut M,
         salt: &[u8],
@@ -41,7 +52,7 @@ fn calculate_block<M: Mac>(
         }
     }
 
-    // Perform all following iterations
+    // Perform all remaining iterations
     for _ in range(2, c) {
         mac.input(scratch);
         mac.raw_result(scratch);
@@ -52,28 +63,51 @@ fn calculate_block<M: Mac>(
     }
 }
 
+/**
+ * Execute the PBKDF2 Key Derivation Function. The Scrypt Key Derivation Function generally provides
+ * better security, so, applications that do not have a requirement to use PBKDF2 specifically
+ * should consider using that function instead.
+ *
+ * # Arguments
+ * * mac - The Pseudo Random Function to use.
+ * * salt - The salt value to use.
+ * * c - The iteration count. Users should carefully determine this value as it is the primary
+ *       factor in determining the security of the derived key.
+ * * output - The output buffer to fill with the derived key value.
+ *
+ */
 pub fn pbkdf2<M: Mac>(mac: &mut M, salt: &[u8], c: uint, output: &mut [u8]) {
-    // TODO - ignoring check that output is not too long
     assert!(c > 0);
 
     let os = mac.output_bytes();
+
+    // A temporary storage array needed by calculate_block. This is really only necessary if c > 1.
+    // Most users of pbkdf2 should use a value much larger than 1, so, this allocation should almost
+    // always be necessary. A big exception is Scrypt. However, this allocation is unlikely to be
+    // the bottleneck in Scrypt performance.
     let mut scratch = vec::from_elem(os, 0u8);
 
+    let mut idx: u32 = 0;
     let mut pos: uint = 0;
-    while pos * os < output.len() {
-        let idx = (pos + 1) as u32;
-        if pos * os + os < output.len() {
-            calculate_block(mac, salt, c, idx, scratch, output.mut_slice(pos * os, pos * os + os));
+    while pos < output.len() {
+        if idx == Bounded::max_value() {
+            fail!("PBKDF2 size limit exceeded.");
+        } else {
+            // The block index starts at 1, so this is supposed to run on the first execution.
+            idx += 1;
+        }
+        let remaining = output.len() - pos;
+        if remaining >= os {
+            calculate_block(mac, salt, c, idx, scratch, output.mut_slice(pos, pos + os));
+            pos += os;
         } else {
             let mut tmp = vec::from_elem(os, 0u8);
             calculate_block(mac, salt, c, idx, scratch, tmp);
-            let remaining = output.len() - pos * os;
-            vec::bytes::copy_memory(output.mut_slice_from(pos * os), tmp, remaining);
+            vec::bytes::copy_memory(output.mut_slice_from(pos), tmp, remaining);
+            break;
         }
-        pos += 1;
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -83,17 +117,6 @@ mod test {
     use hmac::Hmac;
     use sha1::Sha1;
 
-    fn from_str(input: &str) -> ~[u8] {
-        use std::u8;
-        use std::uint;
-        let mut out: ~[u8] = ~[];
-        do uint::range_step(0, input.len(), 2) |i| {
-            out.push(u8::from_str_radix(input.slice(i, i+2), 16).unwrap());
-            true
-        };
-        return out;
-    }
-
     struct Test {
         password: ~[u8],
         salt: ~[u8],
@@ -101,44 +124,54 @@ mod test {
         expected: ~[u8]
     }
 
+    // Test vectors from http://tools.ietf.org/html/rfc6070. The 4th test vector is omitted because
+    // it takes too long to run.
+
     fn tests() -> ~[Test] {
         return ~[
             Test {
                 password: "password".as_bytes().to_owned(),
                 salt: "salt".as_bytes().to_owned(),
                 c: 1,
-                expected: from_str("0c60c80f961f0e71f3a9b524af6012062fe037a6")
+                expected: ~[
+                    0x0c, 0x60, 0xc8, 0x0f, 0x96, 0x1f, 0x0e, 0x71,
+                    0xf3, 0xa9, 0xb5, 0x24, 0xaf, 0x60, 0x12, 0x06,
+                    0x2f, 0xe0, 0x37, 0xa6 ]
             },
             Test {
                 password: "password".as_bytes().to_owned(),
                 salt: "salt".as_bytes().to_owned(),
                 c: 2,
-                expected: from_str("ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957")
+                expected: ~[
+                    0xea, 0x6c, 0x01, 0x4d, 0xc7, 0x2d, 0x6f, 0x8c,
+                    0xcd, 0x1e, 0xd9, 0x2a, 0xce, 0x1d, 0x41, 0xf0,
+                    0xd8, 0xde, 0x89, 0x57 ]
             },
             Test {
                 password: "password".as_bytes().to_owned(),
                 salt: "salt".as_bytes().to_owned(),
                 c: 4096,
-                expected: from_str("4b007901b765489abead49d926f721d065a429c1")
+                expected: ~[
+                    0x4b, 0x00, 0x79, 0x01, 0xb7, 0x65, 0x48, 0x9a,
+                    0xbe, 0xad, 0x49, 0xd9, 0x26, 0xf7, 0x21, 0xd0,
+                    0x65, 0xa4, 0x29, 0xc1 ]
             },
-// This takes too long
-//             Test {
-//                 password: "password".as_bytes().to_owned(),
-//                 salt: "salt".as_bytes().to_owned(),
-//                 c: 16777216,
-//                 expected: from_str("eefe3d61cd4da4e4e9945b3d6ba2158c2634e984")
-//             },
             Test {
                 password: "passwordPASSWORDpassword".as_bytes().to_owned(),
                 salt: "saltSALTsaltSALTsaltSALTsaltSALTsalt".as_bytes().to_owned(),
                 c: 4096,
-                expected: from_str("3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038")
+                expected: ~[
+                    0x3d, 0x2e, 0xec, 0x4f, 0xe4, 0x1c, 0x84, 0x9b,
+                    0x80, 0xc8, 0xd8, 0x36, 0x62, 0xc0, 0xe4, 0x4a,
+                    0x8b, 0x29, 0x1a, 0x96, 0x4c, 0xf2, 0xf0, 0x70, 0x38 ]
             },
             Test {
                 password: ~[112, 97, 115, 115, 0, 119, 111, 114, 100],
                 salt: ~[115, 97, 0, 108, 116],
                 c: 4096,
-                expected: from_str("56fa6aa75548099dcc37d7f03425e0c3")
+                expected: ~[
+                    0x56, 0xfa, 0x6a, 0xa7, 0x55, 0x48, 0x09, 0x9d,
+                    0xcc, 0x37, 0xd7, 0xf0, 0x34, 0x25, 0xe0, 0xc3 ]
             }
         ];
     }
