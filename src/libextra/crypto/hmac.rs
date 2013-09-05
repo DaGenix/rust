1569bb4aa4f329d32d714b2a9127b7e9c0689f00
Palmer Cox <p@lmercox.com>
2013-09-05 00:34:40 -0400

    Update hmac and mac impls.

--- src/libextra/crypto/hmac.rs
+++ src/libextra/crypto/hmac.rs
@@ -8,13 +8,18 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+/*!
+ * This module implements the Hmac function - a Message Authentication Code using a Digest.
+ */
+
 use std::vec;
 
 use digest::Digest;
 use mac::{Mac, MacResult};
 
-
-/// TMP
+/**
+ * The Hmac struct represents an Hmac function - a Message Authentication Code using a Digest.
+ */
 pub struct Hmac<D> {
     priv digest: D,
     priv i_key: ~[u8],
@@ -28,6 +33,9 @@ fn derive_key(key: &mut [u8], mask: u8) {
     }
 }
 
+// The key that Hmac processes must be the same as the block size of the underlying Digest. If the
+// provided key is smaller than that, we just pad it with zeros. If its larger, we hash it and then
+// pad it with zeros.
 fn expand_key<D: Digest>(digest: &mut D, key: &[u8]) -> ~[u8] {
     let bs = digest.block_size();
     let mut expanded_key = vec::from_elem(bs, 0u8);
@@ -48,6 +56,8 @@ fn expand_key<D: Digest>(digest: &mut D, key: &[u8]) -> ~[u8] {
     return expanded_key;
 }
 
+// Hmac uses two keys derived from the provided key - one by xoring every byte with 0x36 and another
+// with 0x5c.
 fn create_keys<D: Digest>(digest: &mut D, key: &[u8]) -> (~[u8], ~[u8]) {
     let mut i_key = expand_key(digest, key);
     let mut o_key = i_key.clone();
@@ -57,7 +67,14 @@ fn create_keys<D: Digest>(digest: &mut D, key: &[u8]) -> (~[u8], ~[u8]) {
 }
 
 impl <D: Digest> Hmac<D> {
-    /// TMP
+    /*
+     * Create a new Hmac instance.
+     *
+     * # Arguments
+     * * digest - The Digest to use.
+     * * key - The key to use.
+     *
+     */
     pub fn new(mut digest: D, key: &[u8]) -> Hmac<D> {
         let (i_key, o_key) = create_keys(&mut digest, key);
         digest.input(i_key);
@@ -67,16 +84,6 @@ impl <D: Digest> Hmac<D> {
             o_key: o_key,
             finished: false
         }
-    }
-
-    /// TMP
-    pub fn reset_key(&mut self, key: &[u8]) {
-        self.digest.reset();
-        let (i_key, o_key) = create_keys(&mut self.digest, key);
-        self.i_key = i_key;
-        self.o_key = o_key;
-        self.digest.input(self.i_key);
-        self.finished = false;
     }
 }
 
@@ -118,7 +125,6 @@ impl <D: Digest> Mac for Hmac<D> {
     fn output_bytes(&self) -> uint { self.digest.output_bytes() }
 }
 
-
 #[cfg(test)]
 mod test {
     use mac::{Mac, MacResult};
@@ -126,23 +132,63 @@ mod test {
     use digest::Digest;
     use md5::Md5;
 
-    fn from_str(input: &str) -> ~[u8] {
-        use std::u8;
-        use std::uint;
-        let mut out: ~[u8] = ~[];
-        do uint::range_step(0, input.len(), 2) |i| {
-            out.push(u8::from_str_radix(input.slice(i, i+2), 16).unwrap());
-            true
-        };
-        return out;
+    struct Test {
+        key: ~[u8],
+        data: ~[u8],
+        expected: ~[u8]
+    }
+
+    // Test vectors from: http://tools.ietf.org/html/rfc2104
+
+    fn tests() -> ~[Test] {
+        return ~[
+            Test {
+                key: ~[0x0b, ..16],
+                data: "Hi There".as_bytes().to_owned(),
+                expected: ~[
+                    0x92, 0x94, 0x72, 0x7a, 0x36, 0x38, 0xbb, 0x1c,
+                    0x13, 0xf4, 0x8e, 0xf8, 0x15, 0x8b, 0xfc, 0x9d ]
+            },
+            Test {
+                key: "Jefe".as_bytes().to_owned(),
+                data: "what do ya want for nothing?".as_bytes().to_owned(),
+                expected: ~[
+                    0x75, 0x0c, 0x78, 0x3e, 0x6a, 0xb0, 0xb5, 0x03,
+                    0xea, 0xa8, 0x6e, 0x31, 0x0a, 0x5d, 0xb7, 0x38 ]
+            },
+            Test {
+                key: ~[0xaa, ..16],
+                data: ~[0xdd, ..50],
+                expected: ~[
+                    0x56, 0xbe, 0x34, 0x52, 0x1d, 0x14, 0x4c, 0x88,
+                    0xdb, 0xb8, 0xc7, 0x33, 0xf0, 0xe8, 0xb3, 0xf6 ]
+            }
+        ];
     }
 
     #[test]
     fn test_hmac_md5() {
-        let mut hmac = Hmac::new(Md5::new(), "key".as_bytes());
-        hmac.input("The quick brown fox jumps over the lazy dog".as_bytes());
-        let result = hmac.result();
-        let expected = MacResult::new_from_owned(from_str("80070713463e7749b90c2dc24911e275"));
-        assert!(result == expected);
+        let tests = tests();
+        for t in tests.iter() {
+            let mut hmac = Hmac::new(Md5::new(), t.key);
+            hmac.input(t.data);
+            let result = hmac.result();
+            let expected = MacResult::new(t.expected);
+            assert!(result == expected);
+        }
+    }
+
+    #[test]
+    fn test_hmac_md5_incremental() {
+        let tests = tests();
+        for t in tests.iter() {
+            let mut hmac = Hmac::new(Md5::new(), t.key);
+            for i in range(0, t.data.len()) {
+                hmac.input(t.data.slice(i, i + 1));
+            }
+            let result = hmac.result();
+            let expected = MacResult::new(t.expected);
+            assert!(result == expected);
+        }
     }
 }
